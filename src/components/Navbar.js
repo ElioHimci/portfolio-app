cde7871df86835d80486e074ccfe7008488aa213
ElioHimci <108124059+ElioHimci@users.noreply.github.com>
2023-07-03 14:05:17 -0400

    initialization

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>Navbar
+    <div className='togleButton'> <button>
+        
+        </button></div>
+    </div>
+  )
+}
+
+export default Navbar
