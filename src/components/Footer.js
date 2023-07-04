e174f9ef3b823ee5f2a6296651990b7daa9079c1
ElioHimci <108124059+ElioHimci@users.noreply.github.com>
2023-07-04 18:47:18 -0400

    home finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import InstagramIcon from "@material-ui/icons/Instagram"
+import EmailIcon from "@material-ui/icons/Email"
 import FacebookIcon from "@material-ui/icons/Facebook"
-import LinkedInIcon from "@material-ui/icons/LinkedIn"
 // import IndeedIcon from "@material-ui/icons/Indeed"
 import "../styles/Footer.css"
 
 function Footer() {
   return (
     <div className='footer'>
-       <div><InstagramIcon/><FacebookIcon/><LinkedInIcon/></div>
-       <p> &copy; 2023 elio</p>
+       <div className='socialMedia'>
+            <InstagramIcon/><EmailIcon/><FacebookIcon/>
+       </div>
+       <p> 
+            &copy; 2023 eliowebdev.com
+       </p>
         </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import LinkedInIcon from "@material-ui/icons/LinkedIn"
+import GitHubIcon from "@material-ui/icons/GitHub"
+import EmailIcon from "@material-ui/icons/Email"
+import '../styles/Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+    <div className='about'>
+      <h2>Hi , my name is Elio</h2>
+      <div className='prompt'> 
+      <p>A software developer with passion for learning and creating</p>
+          <LinkedInIcon/>
+          <GitHubIcon/>
+          <EmailIcon/>
+        </div>
+       </div>
+    <div className='skills'>
+      <h1> Skills </h1>
+      <ol className='list'>
+        <li className='item'>
+          <h2> Front-End</h2>
+          <span> ReactJS, Redux, HTML, CSS, React Native, Flutter, NPM,
+            Bootstrap, MaterialUI, StyledComponents
+          </span>
+        </li>
+        <li className='item'>
+          <h2> Back-End</h2>
+          <span> MySQL, MongoDB, Firebase 
+          </span>
+        </li>
+        <li className='item'>
+          <h2> Languages </h2>
+          <span>Javascript, Typescript, Go</span>
+        </li>
+      </ol>
+    </div>
+    </div>
   )
 }
 
