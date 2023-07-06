42e0e4902f03a9cc8168f84fdfe72c0eb5acbea4
ElioHimci <108124059+ElioHimci@users.noreply.github.com>
2023-07-05 20:08:10 -0400

    created project page and finished with the component and array

---
+++ src/helper/ProjectList.js
@@ -0,0 +1,33 @@
+import Project1 from '../assets/project1.png'
+import Project2 from '../assets/project2.jpg'
+import Project3 from '../assets/project3.jpg'
+import Project4 from '../assets/project4.jpg'
+import Project5 from '../assets/project5.jpg'
+
+export const ProjectList = [
+{
+    name:'Ecommerce',
+    image : Project1,
+},
+{
+    name:'Social Media',
+    image : Project2,
+},
+{
+    name:'Netflix clone',
+    image : Project3,
+},
+{
+    name:'spotify clone',
+    image : Project4,
+},
+{
+    name:'Pets food shop',
+    image : Project5,
+},
+{
+    name:'Pets food shop',
+    image : Project5,
+},
+
+]
